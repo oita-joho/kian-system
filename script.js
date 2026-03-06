6bd1e393e1412eb82ab8ec518f68de4d3cb6f2a3
oita-joho <oitajoho@gmail.com>
2026-03-06 16:38:43 +0900

    Update script.js

--- script.js
+++ script.js
@@ -387,3 +387,20 @@ window.addEventListener("load", ()=>{
     el.addEventListener("change", autoSaveDraft);
   });
 });
+function checkForm(){
+
+  const type = document.getElementById("type").value;
+  const seiriNo = document.getElementById("seiriNo").value.trim();
+
+  if(type===""){
+    alert("区分を選択してください");
+    return false;
+  }
+
+  if(seiriNo===""){
+    alert("整理番号を入力してください");
+    return false;
+  }
+
+  return true;
+}
