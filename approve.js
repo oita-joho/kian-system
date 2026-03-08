df3f84dad9698604f21c7e8e3ced23db781b8764
oita-joho <oitajoho@gmail.com>
2026-03-08 14:38:06 +0900

    Update approve.js

--- approve.js
+++ approve.js
@@ -2,7 +2,7 @@
 // approve.js
 // 承認 + 差し戻し対応版
 // ================================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwdXfLyVByUKJQOey9gbq7_Ra2lvzUu6nDwIbX1HFIUfWNsI7Gp4IbsjKiPpe93j_bd7g/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwgSUeNNjd7wTPQOWoj_1Kb_mu4UiLmysxe5yoHbj2jt3axQ55Yx2oFZ7LiTcCWesGX3w/exec";
 
 function $(id){ return document.getElementById(id); }
 
