544bd1f9333fafd37d07ca352f51a7854a1eb280
oita-joho <oitajoho@gmail.com>
2026-03-08 10:13:14 +0900

    Update approve.js

--- approve.js
+++ approve.js
@@ -1,7 +1,7 @@
 // ================================
 // approve.js
 // ================================
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzQyAkQ0KbrQamPXDB_fPCMGBKouYO2HOJ1nWrTTpJe53BiF1KwFKTdr8e2IXNO-iI2EA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwdXfLyVByUKJQOey9gbq7_Ra2lvzUu6nDwIbX1HFIUfWNsI7Gp4IbsjKiPpe93j_bd7g/exec";
 
 
 function $(id){ return document.getElementById(id); }
